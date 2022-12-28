9413b41686652520c7b015dcd87d78d03f8671c5
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2022-12-27 21:47:38 -0700

    Completed Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "F-150";
+            myCar.Year = 2020;
+
+            Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
+
+            /* can also do it this way
+             * var myCar = new Car()
+             * {
+             *    Make = "Ford",
+             *    Model = "F-150",
+             *    Year = 2020
+             *  };
+             *  */
+
         }
     }
 }
